e0abed01c3e3802db8e44129e66dd51bdf1bb5b0
Haseebullah kehar <159536251+Haseebullahkehar@users.noreply.github.com>
2025-02-15 11:40:08 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treal all JS code  as newer version 
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log( 3 
+    +
+    3)  // code readibility should be maintained 
+
+let name = "Haseeb"
+let age = 18
+let isLoggedIn = false 
+
+
+// number => 2 to power 53
+// BigInt
+// string  => ""
+// boolean => true/false 
+// null => standalone value 
+// undefined => 
+// symbol  => unique 
+
+
+// Object 
+
+console.log(typeof null);
+
+
